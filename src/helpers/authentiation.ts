47e6ca76d8aeb3d26effb7253710c26f445558bb
Calvin Deng <dengcalvin@outlook.com>
2023-07-24 15:01:32 -0700

    documented helper files

--- src/helpers/authentiation.ts
+++ src/helpers/authentiation.ts
@@ -1,14 +1,41 @@
 import * as LocalAuthentication from "expo-local-authentication";
 import { Linking, Platform } from "react-native";
 
+/**
+ * Checks if local authentication (e.g., fingerprint or face recognition) is supported on the device.
+ *
+ * @async
+ * @function
+ * @returns {Promise<boolean>} A Promise that resolves to a boolean value indicating whether local authentication is supported.
+ *
+ * @throws {Error} If there is an error while checking for hardware support.
+ */
 const localAuthSupported = async () => {
     return await LocalAuthentication.hasHardwareAsync();
 };
 
+/**
+ * Checks if the user has enrolled in local authentication (e.g., fingerprint or face recognition).
+ *
+ * @async
+ * @function
+ * @returns {Promise<boolean>} A Promise that resolves to a boolean value indicating whether the user has enrolled in local authentication.
+ *
+ * @throws {Error} If there is an error while checking for local authentication enrollment.
+ */
 const localAuthEnrolled = async () => {
     return await LocalAuthentication.isEnrolledAsync();
 };
 
+/**
+ * Initiates local authentication (e.g., fingerprint or face recognition) for the user.
+ *
+ * @async
+ * @function
+ * @returns {Promise<LocalAuthentication.LocalAuthenticationResult>} A Promise that resolves to the result of the local authentication process.
+ *
+ * @throws {Error} If there is an error while performing local authentication or if the user cancels the authentication process.
+ */
 const localAuthTrigger = async () => {
     const options: LocalAuthentication.LocalAuthenticationOptions = {
         promptMessage: "Please enable local authentication",
@@ -17,25 +44,16 @@ const localAuthTrigger = async () => {
     return await LocalAuthentication.authenticateAsync(options);
 };
 
-const listLocalAuthType = async () => {
-    const supportTypes =
-        await LocalAuthentication.supportedAuthenticationTypesAsync();
-    console.log("supportTypes: ", supportTypes);
-
-    if (
-        supportTypes.includes(
-            LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION
-        )
-    ) {
-        console.log("Facial recognition");
-    }
-};
-
-const localAuthLevel = async () => {
-    const level = await LocalAuthentication.getEnrolledLevelAsync();
-    console.log("auth level: ", level);
-};
-
+/**
+ * Initiates the local authentication process for the user and updates the authentication state.
+ *
+ * @async
+ * @function
+ * @param {React.Dispatch<React.SetStateAction<boolean>>} authCallback - The state update callback to set the authentication state.
+ * @returns {Promise<void>} A Promise that resolves after the local authentication process is completed or an error occurs.
+ *
+ * @throws {Error} If there is an error while performing local authentication.
+ */
 const localAuthInit = async (
     authCallback: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
--- src/helpers/todoItems.ts
+++ src/helpers/todoItems.ts
@@ -1,16 +1,35 @@
 import { TodoItemType } from "../types";
 
+/**
+ * Add a new item to the todoItems array and update it with the new item.
+ *
+ * @param {TodoItemType[]} todoItems - The current array of todo items.
+ * @param {string} label - The label of the new item to be added.
+ * @param {React.Dispatch<React.SetStateAction<TodoItemType[]>>} updateCallback - The state update callback to set the updated todoItems.
+ * @returns {void}
+ */
 const addNewItem = (
     todoItems: TodoItemType[],
     label: string,
     updateCallback: React.Dispatch<React.SetStateAction<TodoItemType[]>>
 ) => {
+    // use uuid instead of relying on timestamps when more package is added
     const id = Date.now().toFixed(0);
     const map = new Map(todoItems.map((item) => [item.id, item]));
     map.set(id, { label: label, id: id });
     updateCallback(Array.from(map.values()));
 };
 
+/**
+ * Selects an item from the todoItems array based on its id and calls the selectCallback with the selected item.
+ * If the item with the given id is not found in the array, it throws an error with the message "TARGET_NOT_FOUND".
+ *
+ * @param {TodoItemType[]} todoItems - The array of todo items to search for the target item.
+ * @param {string} id - The id of the item to be selected.
+ * @param {React.Dispatch<React.SetStateAction<TodoItemType | undefined>>} selectCallback - The state update callback to set the selected item.
+ * @throws {Error} Throws an error with the message "TARGET_NOT_FOUND" if the item with the given id is not found.
+ * @returns {void}
+ */
 const selectItemById = (
     todoItems: TodoItemType[],
     id: string,
@@ -18,10 +37,26 @@ const selectItemById = (
         React.SetStateAction<TodoItemType | undefined>
     >
 ) => {
-    const target = todoItems.find((item) => item.id == id);
-    selectCallback(target);
+    const map = new Map(todoItems.map((item) => [item.id, item]));
+    const target = map.get(id);
+    if (target) {
+        selectCallback(target);
+    } else {
+        throw new Error("TARGET_NOT_FOUND");
+    }
 };
 
+/**
+ * Updates the label of an item in the todoItems array based on its id and calls the updateCallback with the updated array.
+ * If the item with the given id is not found in the array, it throws an error with the message "TARGET_NOT_FOUND".
+ *
+ * @param {TodoItemType[]} todoItems - The array of todo items to search for the target item.
+ * @param {string} id - The id of the item to be updated.
+ * @param {string} updatedLabel - The updated label for the item.
+ * @param {React.Dispatch<React.SetStateAction<TodoItemType[]>>} updateCallback - The state update callback to set the updated array.
+ * @throws {Error} Throws an error with the message "TARGET_NOT_FOUND" if the item with the given id is not found.
+ * @returns {void}
+ */
 const updateItemById = (
     todoItems: TodoItemType[],
     id: string,
@@ -34,10 +69,20 @@ const updateItemById = (
         map.set(id, { ...target, label: updatedLabel });
         updateCallback(Array.from(map.values()));
     } else {
-        // TODO: error callback
+        throw new Error("TARGET_NOT_FOUND");
     }
 };
 
+/**
+ * Removes an item from the todoItems array based on its id and calls the updateCallback with the updated array.
+ * If the item with the given id is not found in the array, it throws an error with the message "TARGET_NOT_FOUND".
+ *
+ * @param {TodoItemType[]} todoItems - The array of todo items to search for the target item.
+ * @param {string} id - The id of the item to be removed.
+ * @param {React.Dispatch<React.SetStateAction<TodoItemType[]>>} updateCallback - The state update callback to set the updated array.
+ * @throws {Error} Throws an error with the message "TARGET_NOT_FOUND" if the item with the given id is not found.
+ * @returns {void}
+ */
 const removeItemById = (
     todoItems: TodoItemType[],
     id: string,
@@ -49,7 +94,7 @@ const removeItemById = (
         map.delete(id);
         updateCallback(Array.from(map.values()));
     } else {
-        // TODO: error callback
+        throw new Error("TARGET_NOT_FOUND");
     }
 };
 
