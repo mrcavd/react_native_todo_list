e76cc50d20c4eb73998073ccca7742e99d4a1595
Calvin Deng <dengcalvin@outlook.com>
2023-07-24 00:03:18 -0700

    Action Button

---
+++ src/features/universal/ActionButton.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Text, TextStyle, TouchableOpacity, ViewProps } from "react-native";
+import { TouchableOpacityOnPress } from "../../types";
+import { BUTTON_STYLE } from "../../constants/styles/buttons/styles";
+
+interface ConfirmButtonProps {
+    label: string;
+    onPress: TouchableOpacityOnPress;
+    buttonStyle?: ViewProps["style"];
+    textStyle?: TextStyle;
+}
+
+const ActionButton: React.FC<ConfirmButtonProps> = ({
+    label,
+    onPress,
+    buttonStyle,
+    textStyle,
+}) => {
+    return (
+        <TouchableOpacity
+            onPress={onPress}
+            style={buttonStyle || BUTTON_STYLE.actionButtonContainer}
+        >
+            <Text style={textStyle || BUTTON_STYLE.actionButtonLabel}>
+                {label}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+export default ActionButton;
