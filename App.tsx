f8f928fa977000fc179c9253859df3d8407534bc
Calvin Deng <dengcalvin@outlook.com>
2023-07-24 17:03:13 -0700

    styling

--- App.tsx
+++ App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { SafeAreaView, StatusBar, useColorScheme } from "react-native";
+import { SafeAreaView, StatusBar, ViewProps } from "react-native";
 import { TodoHome } from "./src/features";
 import { COLORS } from "./src/constants";
 
 function App(): JSX.Element {
-    const isDarkMode = useColorScheme() === "dark";
-
     const appContainer = {
         flex: 1,
-    };
+        backgroundColor: COLORS.Neutral.OffWhite,
+    } as ViewProps;
 
     return (
         <SafeAreaView style={appContainer}>
             <StatusBar
-                barStyle={isDarkMode ? "light-content" : "dark-content"}
+                barStyle={"dark-content"}
                 backgroundColor={COLORS.Neutral.Black}
             />
             <TodoHome />
